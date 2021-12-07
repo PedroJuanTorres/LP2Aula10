351dbe22c9744d601c729d28654b7e6ccb031d6c
Pedro Torres <pedrojuantorres.info@gmail.com>
2021-12-07 15:32:58 +0000

    Solve Ex1

---
+++ FrogRace/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace FrogRace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Race);
+            t1.Name = "Frog 1";
+
+            Thread t2 = new Thread(Race);
+            t2.Name = "Frog 2";
+
+            Thread t3 = new Thread(Race);
+            t3.Name = "Frog 3";
+
+
+            t1.Start(1);
+            t2.Start(2);
+            t3.Start(3);
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+        }
+
+        private static void Race(object n)
+        {
+            int i = (int)n;    
+
+            Random rnd = new Random(i);
+
+            for (int j = 0; j < 10; j++)
+            {
+                Console.WriteLine($"Rã {i} deu salto {j} (Nome da thread: {Thread.CurrentThread.Name}");
+                Thread.Sleep(rnd.Next(1001));
+            }
+        }
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(CountToNumber);
+            thread1.Start(10);
+            Thread thread2 = new Thread(CountToNumber);
+            thread2.Start(100);
+            thread1.Join();
+            thread2.Join();
+        }
+
+        private static void CountToNumber(object input)
+        {
+            int n = (int)input;
+            for (int index = 0; index < n; index++)
+                Console.WriteLine($"{index++} ({Thread.CurrentThread.Name})");
+        }
+    }
+}
